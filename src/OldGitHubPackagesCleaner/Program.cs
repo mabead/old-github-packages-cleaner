7323d8be4f6e55af28e84ff6a751d6bb7e1aaaa9
Maxime Beaudry <mabead@users.noreply.github.com>
2020-05-31 16:19:24 -0400

    create build process (#1)

---
+++ src/OldGitHubPackagesCleaner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OldGitHubPackagesCleaner
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub actions!");
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"  arg: {arg}");
+                await Task.Delay(50);
+            }
+        }
+    }
+}
